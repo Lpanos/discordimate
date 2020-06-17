fa755a5f9b67e4985b278620ae10363f34916baf
Landon Panos <lpanos@bailhotline.net>
2020-06-17 12:02:19 -0700

    broken code

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
+
+module.exports = { 
+
+animate : async function animate() {
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('message', message => {
-module.exports = async function animate() {
-    
-    
-    function animateSleep(ms) {
-        return new animatePromise(resolve => animateSetTimeout(resolve, ms));
+
+            function animateSleep(ms) {
+                return new animatePromise(resolve => animateSetTimeout(resolve, ms));
+            }
+            const animateWait = ms => new animatePromise(resolve => animateSetTimeout(resolve, ms));
+            const SentMessage = await message.channel.send(arguments[0]);
+            // continue editing with the remaining args
+            for (let i=1; i < arguments.length; i++) {
+                await animateWait(1000);
+                await sentMessage.edit(arguments[i]);
+            }
+        }
     }
-
-    const animateWait = ms => new animatePromise(resolve => animateSetTimeout(resolve, ms));
-
-    const SentMessage = await message.channel.send(arguments[0]);
-    // continue editing with the remaining args
-    for (let i=1; i < arguments.length; i++) {
-        await animateWait(1000);
-        await sentMessage.edit(arguments[i]);
-    }
-
-
-}
-
 })
+            
