b9e8432a23f3393878e82cdd4f9b8dca77d051bc
Landon Panos <lpanos@bailhotline.net>
2020-06-18 11:24:03 -0700

    IF THIS WORKS I AM SO SMART

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 module.exports = async function animate() {
-
+messgage = (arguments[0])
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
     const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
             // send with the first argument
-    const sentMessage = await message.channel.send(arguments[0]);
+    const sentMessage = await message.channel.send(arguments[1]);
             // continue editing with the remaining args
-    for (let i=1; i < arguments.length; i++) {
+    for (let i=2; i < arguments.length; i++) {
             await wait(1000);
             await sentMessage.edit(arguments[i]);
     }
