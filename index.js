950bbead082dbdd3ae9d8d75ead7714ebbead77a
Landon Panos <lpanos@bailhotline.net>
2020-06-18 08:37:25 -0700

    It wasnt figured out

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 module.exports = { 
 
 animate : async function animate() {
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('message', message => {
 
             function animateSleep(ms) {
                 return new animatePromise(resolve => animateSetTimeout(resolve, ms));
@@ -17,7 +15,6 @@ client.on('message', message => {
                 await animateWait(1000);
                 await sentMessage.edit(arguments[i]);
             }
-        }
-    )}
+    }
 }
             
