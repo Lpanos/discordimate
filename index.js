90b0225834d9c9817c3ffc3e4dfc4c93fa9458c6
Landon Panos <lpanos@bailhotline.net>
2020-07-03 08:52:00 -0700

    added errors

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 module.exports = async function animate() {
 
-        // if(Array.isArray(arguments[0]) == false){
-        //         throw new Error("The first variable is not set to your message array ('message' or 'msg')");
-        // }
-
-    message = (arguments[0])
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
-    const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
-            // send with the first argument
-    const sentMessage = await message.channel.send(arguments[1]);
-            // continue editing with the remaining args
-    for (let i=2; i < arguments.length; i++) {
-            await wait(1000);
-            await sentMessage.edit(arguments[i]);
-    }
+        if ((arguments[0]) == null) {
+                throw new Error("The first variable is not set to your message array ('message' or 'msg')")
+            }
+        else if((arguments[0]) == undefined) {
+                throw new Error("The first variable is not set to your message array ('message' or 'msg')")
+            }
+        else{
+                message = (arguments[0])
+                function sleep(ms) {
+                        return new Promise(resolve => setTimeout(resolve, ms));
+                }
+                const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+                        // send with the first argument
+                const sentMessage = await message.channel.send(arguments[1]);
+                        // continue editing with the remaining args
+                for (let i=2; i < arguments.length; i++) {
+                        await wait(1000);
+                        await sentMessage.edit(arguments[i]);
+                        }
+        }
 }
